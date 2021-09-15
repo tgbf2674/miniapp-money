15e539eb61bd655568e343c15bb5117ee947d11d
maqing <136518041@qq.com>
2021-09-15 13:13:02 +0800

    init

---
+++ components/tabs.vue
@@ -0,0 +1,76 @@
+<template>
+	<view class="tabsWrapper">
+		<ul class="tabs">
+			<li v-for="(item,index) in data_source" :key="item.value" class="tabs-item" :class="{selected: item.value === value}" @click="select(item)">
+				{{item.text}}
+			</li>
+		</ul>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"tabs",
+		props:{
+			data_source:{
+				type: Array
+			},
+			value: {
+				type:String
+			}
+		},
+		methods:{
+			select(item){
+				this.$emit('update:value',item.value)
+			}
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.tabsWrapper {
+		width: 100vw;
+		background: $main-color;
+		height: 60px;
+		display: flex;
+		align-items: center;
+		.tabs {
+			display: flex;
+			text-align: center;
+			font-size: 20px;
+			color: white;
+			width: 40vw;
+			margin: 0 auto;
+			border: 1px solid #303030;
+			border-radius: 5px;
+			border-collapse: collapse;
+			li:first-of-type {
+				width: 50vw;
+				height: 32px;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				position: relative;
+				border-radius: 4px 0 0 4px;
+			}
+			li:last-of-type {
+				width: 50vw;
+				height: 32px;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				position: relative;
+				border-radius: 0 4px 4px 0;
+			}
+			& .selected {
+				background: #303030;
+				color: white;
+			}
+		}
+	}
+</style>
---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="content">
+			<tabs :data_source="recordTypeList" :value.sync="record.type"></tabs>
+	</view>
+		
+</template>
+
+<script>
+	export default {
+		data() {
+					return {
+						title: '果果记账',
+						recordTypeList: [{
+								text: '支出',
+								value: '-'
+							},
+							{
+								text: '收入',
+								value: '+'
+							},
+						],
+						record: {
+							tags: [],
+							notes: '',
+							type: '-'
+						}
+					}
+				},
+				onLoad() {
+				},
+				methods: {
+				}
+	}
+</script>
+
+<style lang="scss">
+.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
